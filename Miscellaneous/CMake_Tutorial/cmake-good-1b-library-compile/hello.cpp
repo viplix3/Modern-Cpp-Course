991696e1629782f5a68070108652aa64c834c670
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-12 18:25:00 +0530

    Added CMake tutorial exersices

---
+++ Miscellaneous/CMake_Tutorial/cmake-good-1b-library-compile/hello.cpp
@@ -0,0 +1,5 @@
+#include "hello.hpp"
+
+void hello::say_hello(){
+	std::cout << "Hello CMake library" << std::endl;
+}
---
+++ Miscellaneous/CMake_Tutorial/cmake-good-1c-subdirectories/say-hello/src/hello.hpp
@@ -0,0 +1,12 @@
+#ifndef HELLO_HPP_
+#define HELLO_HPP_
+
+#include <iostream>
+
+namespace hello {
+
+void say_hello();
+
+}
+
+#endif
