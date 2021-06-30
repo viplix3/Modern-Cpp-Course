6b9d332a4e470e7b7f965439fc12fdd9ef832a24
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-30 07:29:54 +0530

    Basic textual profiler

---
+++ Miscellaneous/Cpp_Misc/main/src/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <stdlib.h>
+
+#include "textual_profiler.h"
+
+int main() {
+	{
+		TextualProfiler::Profiler time;
+		printf("\nHello World using printf\n");
+	}
+
+	{
+		TextualProfiler::Profiler time;
+		std::cout << "\nHello world using std::cout\n";
+	}
+
+	{
+		TextualProfiler::Profiler time;
+		std::cout << std::endl << "Hello world using std::cout and std::endl" << std::endl;
+	}
+
+	return EXIT_SUCCESS;
+}
