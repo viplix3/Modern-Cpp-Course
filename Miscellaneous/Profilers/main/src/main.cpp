6f9b127d3142132fdbc3ed21ff0d577b2e7ba161
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-06-30 09:40:53 +0530

    Visual profiler. Open results.json in chrome://tracing/

--- Miscellaneous/Profilers/main/src/main.cpp
+++ Miscellaneous/Profilers/main/src/main.cpp
@@ -2,9 +2,18 @@
 #include <stdlib.h>
 
 #include "textual_profiler.h"
-#include "../../visual_profiler/include/visual_profiler.h"
+#include "visual_profiler.h"
+
+void print_hello_world() {
+	PROFILE_FUNCTION();
+	TextualProfiler::Profiler time("print_hello_world()");
+	std::cout << "\nHello World" << std::endl;
+}
 
 int main() {
+	Instrumentor::Get().BeginSession("main", "/home/viplix3/GitHub/Modern-Cpp-Course/Miscellaneous/Profilers/main/results.json");
+	print_hello_world();
+
 	{
 		PROFILE_SCOPE("printf");
 		TextualProfiler::Profiler time;
@@ -12,14 +21,18 @@ int main() {
 	}
 
 	{
+		PROFILE_SCOPE("std::cout");
 		TextualProfiler::Profiler time("std::cout");
 		std::cout << "\nHello world using std::cout\n";
 	}
 
 	{
+		PROFILE_SCOPE("std::endl");
 		TextualProfiler::Profiler time;
 		std::cout << std::endl << "Hello world using std::cout and std::endl" << std::endl;
 	}
+
+	Instrumentor::Get().EndSession();
 
 	return EXIT_SUCCESS;
 }
