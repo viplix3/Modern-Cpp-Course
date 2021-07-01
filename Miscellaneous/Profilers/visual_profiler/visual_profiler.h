ff061419b752423aede99294d2b6fb66f4e2ffdf
Vipin Sharma <vipin.sharma2024@gmail.com>
2021-07-01 05:37:31 +0530

    Code refactoring for InstrumentationTimer

--- Miscellaneous/Profilers/visual_profiler/visual_profiler.h
+++ Miscellaneous/Profilers/visual_profiler/visual_profiler.h
@@ -113,9 +113,15 @@ public:
 
 class InstrumentationTimer
 {
+private:
+    ProfileResult m_profResult;
+
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
+    bool m_Stopped;
+
 public:
-    InstrumentationTimer(const char* name)
-        : m_Name(name), m_Stopped(false)
+    InstrumentationTimer(const std::string& name)
+        : m_profResult({name, 0, 0, 0}), m_Stopped(false)
     {
         m_StartTimepoint = std::chrono::high_resolution_clock::now();
     }
@@ -130,16 +136,12 @@ public:
     {
         auto endTimepoint = std::chrono::high_resolution_clock::now();
 
-        long long start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
-        long long end = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
+        m_profResult.Start = std::chrono::time_point_cast<std::chrono::microseconds>(m_StartTimepoint).time_since_epoch().count();
+        m_profResult.End = std::chrono::time_point_cast<std::chrono::microseconds>(endTimepoint).time_since_epoch().count();
 
-        uint32_t threadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
-        Instrumentor::Get().WriteProfile({ m_Name, start, end, threadID });
+        m_profResult.ThreadID = std::hash<std::thread::id>{}(std::this_thread::get_id());
+        Instrumentor::Get().WriteProfile(m_profResult);
 
         m_Stopped = true;
     }
-private:
-    const char* m_Name;
-    std::chrono::time_point<std::chrono::high_resolution_clock> m_StartTimepoint;
-    bool m_Stopped;
 };
